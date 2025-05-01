80fb20d438797b72dac0f67751b7c1e21aa64388
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2025-05-01 13:31:19 +0530

    renamed:    ntxsystems-landing-page.png -> assets/ntxsystems-landing-page.png new file:   includes/footer.php new file:   includes/header.php new file:   includes/navbar.php new file:   index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+<div style="max-width: 100%; max-height: 100%; overflow: hidden; display: flex; justify-content: center; align-items: center;">
+    <img src="assets/ntxsystems-landing-page.png" alt="" style="width: 100%;">
+</div>
+<?php include 'includes/footer.php'; ?>
