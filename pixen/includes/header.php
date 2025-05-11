3a3cf20777006820a69266ea1cab367642b294fa
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2025-05-11 13:16:11 +0530

    new file:   pixen/assets/banners/banner_1.png new file:   pixen/assets/download/pixen.exe new file:   pixen/assets/logo/pixen_logo.png new file:   pixen/assets/logo/pixen_logo_exeicon.ico new file:   pixen/assets/logo/pixen_logo_icon.png new file:   pixen/assets/styles.css new file:   pixen/includes/footer.php new file:   pixen/includes/header.php new file:   pixen/includes/navbar.php new file:   pixen/index.php

---
+++ pixen/includes/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ============ TITLE ============ -->
+    <title>Pixen Lights - Immersive Experience</title>
+
+    <!-- ============ BOOTSTRAP ============ -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
+    <!-- ============ FAVICON ============ -->
+    <link rel="shortcut icon" href="assets/logo/pixen_logo_exeicon.ico" type="image/x-icon">
+
+    <!-- ============ STYLESHEET ============ -->
+    <link rel="stylesheet" href="assets/styles.css">
+
+    <!-- ============ GOOGLE FONTS ============ -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
---
+++ pixen/index.php
@@ -0,0 +1,8 @@
+<?php
+include 'includes/header.php';
+include 'includes/navbar.php'; ?>
+<div style="max-width: 100%">
+    <img style="width: 100%; height: auto" src="assets/banners/banner_1.png" alt="">
+</div>
+
+<?php include 'includes/footer.php'; ?>
